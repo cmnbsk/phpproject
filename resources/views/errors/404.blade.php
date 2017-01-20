05782fab9e8e303553dd4b546b216e678f1b67a9
Konrad Depta <konradd93@gmail.com>
2017-01-20 15:55:25 +0100

    kondziu

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,9 +2,9 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Error 404 nie odnaleziono</title>
+        <title>Error 404 - Nie odnaleziono</title>
     </head>
     <body>
-        <h2>Strona nie istnieje</h2>
+        <h2>Strona nie istnieje ;(</h2>
     </body>
 </html>
