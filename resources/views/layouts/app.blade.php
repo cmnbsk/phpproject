b5f92b004087002cd4adc1404c3c358d2fd2aaef
darkbluegt <darkbluegt@gmail.com>
2017-01-22 00:49:17 +0100

    dodany js href

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <link href="./css/app.css" rel="stylesheet">
 
     <!-- Scripts -->
+    <script src="../js/app.js"></script>
     <script>
         window.Laravel = <?php echo json_encode([
             'csrfToken' => csrf_token(),
